c1632815d6377e3756137411f55244cb23071427
Samuel Lord <sllord@icloud.com>
2018-03-07 20:20:24 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt remove -y pcmanfm
+sudo apt remove -y pcmanfm gnome-mpv audacious xfburn abiword gnumeric
 sudo apt install --no-install-recommends -y i3 compton git xfce4-panel nitrogen thunar
 git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
@@ -8,4 +8,5 @@ cp i3 -r ~/.config/i3
 cp compton -r ~/.config/compton
 sudo mkdir /wallpapers
 sudo cp -r wallpapers /wallpapers
+sudo apt install -y pingus tuxpaint supertuxkart steam vlc brasero libreoffice krita gimp
 echo Done!
