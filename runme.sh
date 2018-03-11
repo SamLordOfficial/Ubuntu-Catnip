6cfe6fbd45d61783c81ccfe3b0b18b7f8c256b82
Samuel Lord <sllord@icloud.com>
2018-03-10 23:27:35 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -12,5 +12,6 @@ sudo mkdir /wallpapers
 sudo chmod 777 /wallpapers
 sudo cp -r wallpapers /
 sudo apt install --no-install-recommends -y pingus tuxpaint tuxpaint-stamps supertuxkart steam vlc brasero libreoffice krita gimp gdebi openshot virtualbox shotwell wine-stable ttf-mscorefonts-installer xfce4 net-tools
+sudo apt install lxterminal lxappearance
 wget https://github.com/sparksammy/YABAH/raw/master/hosts -O ~/YABAH && cat ~/YABAH | sudo tee -a /etc/hosts && rm ~/YABAH
 echo Done!
