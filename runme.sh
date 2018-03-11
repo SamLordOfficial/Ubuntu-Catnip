d5e16c5b4d6578d0755d228c2da5cf8b5aa42908
Samuel Lord <sllord@icloud.com>
2018-03-10 23:33:22 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -11,7 +11,7 @@ cp compton.conf ~/.config/compton.conf
 sudo mkdir /wallpapers
 sudo chmod 777 /wallpapers
 sudo cp -r wallpapers /
-sudo apt install --no-install-recommends -y pingus tuxpaint tuxpaint-stamps supertuxkart steam vlc brasero libreoffice krita gimp gdebi openshot virtualbox shotwell wine-stable ttf-mscorefonts-installer xfce4 net-tools
+sudo apt install --no-install-recommends -y pingus tuxpaint supertux2 minetest tuxpaint-stamps supertuxkart steam vlc brasero libreoffice krita gimp gdebi openshot virtualbox shotwell wine-stable ttf-mscorefonts-installer xfce4 net-tools
 sudo apt install lxterminal lxappearance
 wget https://github.com/sparksammy/YABAH/raw/master/hosts -O ~/YABAH && cat ~/YABAH | sudo tee -a /etc/hosts && rm ~/YABAH
 echo Done!
