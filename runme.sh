719aad1e58c94d3a535fe1d0bb038f19065e8ae5
Samuel Lord <sllord@icloud.com>
2018-03-10 22:04:02 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt remove -y pcmanfm gnome-mpv audacious xfburn abiword gnumeric
-sudo apt install --no-install-recommends -y i3 compton git xfce4-panel nitrogen thunar xfce4-session
+sudo apt install --no-install-recommends -y i3 compton git xfce4-panel nitrogen thunar xfce4-session rhythmbox
 git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
 sudo cp i3 -r /etc/
@@ -9,6 +9,6 @@ cp compton.conf ~/.config/compton.conf
 sudo mkdir /wallpapers
 sudo chmod 777 /wallpapers
 sudo cp -r wallpapers /
-sudo apt install -y pingus tuxpaint supertuxkart steam vlc brasero libreoffice krita gimp gdebi openshot virtualbox wine-stable ttf-mscorefonts-installer net-tools
+sudo apt install --no-install-recommends -y pingus tuxpaint tuxpaint-stamps supertuxkart steam vlc brasero libreoffice krita gimp gdebi openshot virtualbox shotwell wine-stable ttf-mscorefonts-installer xfce4 net-tools
 wget https://github.com/sparksammy/YABAH/raw/master/hosts -O ~/YABAH && cat ~/YABAH | sudo tee -a /etc/hosts && rm ~/YABAH
 echo Done!
