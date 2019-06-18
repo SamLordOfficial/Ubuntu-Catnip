e487ee9dfa6f1d3b6a77e26172d42ac1e639d934
Samuel Lord <sllord@icloud.com>
2019-06-17 22:51:19 -0500

    Update runme-xubuntu-lmxfce.sh

--- runme-xubuntu-lmxfce.sh
+++ runme-xubuntu-lmxfce.sh
@@ -28,6 +28,6 @@ sudo apt-get update && sudo apt-get install -y obs-studio
 hostnameline="127.0.1.1 $(cat /etc/hostname)"
 echo $hostnameline > /etc/hosts
 wget https://github.com/sparksammy/YABAH/raw/master/hosts -O ~/YABAH && cat ~/YABAH | sudo tee -a /etc/hosts && rm ~/YABAH
-sudo ln -s /usr/bin/man /usr/bin/genderless-attack-helicopter﻿ 
+sudo ln -s /usr/bin/man /usr/bin/woman﻿ 
 sudo apt install -y pandoc texlive lmodern
 echo Done! Please login to i3.
