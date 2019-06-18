e85ded19e5510a699f0a6f099b60cc4ce0155cac
Samuel Lord <sllord@icloud.com>
2019-06-18 17:04:49 -0500

    Update runme-xubuntu-lmxfce.sh

--- runme-xubuntu-lmxfce.sh
+++ runme-xubuntu-lmxfce.sh
@@ -30,5 +30,5 @@ hostnameline="127.0.1.1 $(cat /etc/hostname)"
 echo $hostnameline > /etc/hosts
 wget https://github.com/sparksammy/YABAH/raw/master/hosts -O ~/YABAH && cat ~/YABAH | sudo tee -a /etc/hosts && rm ~/YABAH
 sudo ln -s /usr/bin/man /usr/bin/woman﻿ 
-sudo apt install -y pandoc texlive lmodern
+sudo apt install -y pandoc texlive lmodern nitrogen
 echo Done! Please login to i3.
