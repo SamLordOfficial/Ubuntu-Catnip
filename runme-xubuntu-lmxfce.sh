9b4a1f33b73ddc9e0dcbe4cc9fa802d79a99fabf
Samuel Lord <sllord@icloud.com>
2019-06-25 16:15:31 -0500

    Update runme-xubuntu-lmxfce.sh

--- runme-xubuntu-lmxfce.sh
+++ runme-xubuntu-lmxfce.sh
@@ -1,3 +1,4 @@
+sudo apt install -y imagemagick feh xdotool
 sudo add-apt-repository ppa:kubuntu-ppa/backports 
 sudo apt update
 sudo apt install -y git
@@ -12,13 +13,14 @@ sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ xenial ma
 sudo apt remove -y pcmanfm gnome-mpv audacious xfburn abiword gnumeric nautilus gdm remmina xed pidgin xfce4-terminal
 git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
-cp i3 ~/.config/i3
-cp i3 -r ~/.config/i3/*
-cp gtk-2.0 ~/.config/gtk-2.0/*
-cp gtk-3.0 ~/.config/gtk-3.0/*
+cp i3/* ~/.config/i3
+cp gtk-2.0/* ~/.config/gtk-2.0/
+cp gtk-3.0/* ~/.config/gtk-3.0/
 cp xfce-perchannel-xml -r ~/.config/xfce-perchannel-xml
 sudo cp sparksammy-logout /bin/sparksammy-logout
 sudo chmod 777 /bin/sparksammy-logout
+sudo cp lock/* /bin/
+sudo chmod 777 /bin/lock
 sudo cp set-wallpaper /bin/set-wallpaper
 sudo chmod 777 /bin/set-wallpaper
 sudo mkdir /wallpapers
